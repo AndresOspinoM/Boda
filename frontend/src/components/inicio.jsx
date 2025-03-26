27c54add831ac6a1beb69245a38b3591a6d17a38
AndresOspino <akmejia522@gmail.com>
2025-03-25 22:48:31 -0500

    Estructura principal del proyecto

---
+++ frontend/src/components/inicio.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Welcome = () => {
+    return (
+        <div className="container mt-5">
+            <h1 className="text-center"> Bienvenidos a la boda </h1>
+            <p className="text-center">Te invitamos a nuestra boda</p>
+        </div>
+    );
+};
+
+export default Welcome;
